3782d522dc222467c8993be69bfcf68ec29b552b
Gragit <gme2468@gmail.com>
2020-10-26 14:46:57 +0800

    minor changes

--- index.js
+++ index.js
@@ -44,46 +44,43 @@ inquirer.prompt([
   },
   {
     type: "list",
-    message: "What is your preferred method of communication?",
-    name: "contact",
+    name: "license",
+    message: "Select a license:",
     choices: [
-      "email",
-      "phone",
-      "telekinesis"
+      "MIT",
+      "Apache",
+      "ISC",
+      "None"
     ]
   }
 ]).then((answers) => {
+
+    if (answers.license == "MIT") {
+      licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (answers.license == "Apache") {
+      licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    } else if (answers.license == "ISC") {
+      licenseBadge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+    } else {
+      licenseBadge = ""
+    }
+  
   fs.writeFileSync("README.md", "# " + answers.projecttitle + '\n' + '\n');
+  fs.appendFileSync("README.md", licenseBadge + '\n' + '\n');
 
   fs.appendFileSync("README.md", "## " + "Description" + '\n' + answers.description + '\n' + '\n');
 
   fs.appendFileSync("README.md", "## " + "Table of Contents" + '\n' + "[Installation]" + "(#installation)" + '\n' + '\n');
-  fs.appendFileSync("README.md", "[Usage]" + "(#usage)" + '\n' + '\n');
-  fs.appendFileSync("README.md", "[Contribution Guidelines]" + "(#contribution-guidelines)" + '\n' + '\n');
-  fs.appendFileSync("README.md", "[Test Instructions]" + "(#test-instructions)" + '\n' + '\n');
-  fs.appendFileSync("README.md", "[Questions]" + "(#questions)" + '\n' + '\n');
+  fs.appendFileSync("README.md", "* [Usage]" + "(#usage)" + '\n' + '\n');
+  fs.appendFileSync("README.md", "* [Contribution Guidelines]" + "(#contribution-guidelines)" + '\n' + '\n');
+  fs.appendFileSync("README.md", "* [Test Instructions]" + "(#test-instructions)" + '\n' + '\n');
+  fs.appendFileSync("README.md", "* [Questions]" + "(#questions)" + '\n' + '\n');
 
   fs.appendFileSync("README.md", "## " + "Installation" + '\n' + answers.installation + '\n' + '\n');
   fs.appendFileSync("README.md", "## " + "Usage" + '\n' + answers.usage + '\n' + '\n');
   fs.appendFileSync("README.md", "## " + "Contribution Guidelines" + '\n' + answers.contributionguidelines + '\n' + '\n');
   fs.appendFileSync("README.md", "## " + "Test Instructions" + '\n' + answers.tests + '\n' + '\n');
+  fs.appendFileSync("README.md", "## " + "License" + '\n' + "This application is covered under the " + answers.license + " license." + '\n' + '\n');
   fs.appendFileSync("README.md", "## " + "Questions" + '\n' + "My GitHub: " + ("https://github.com/" + answers.github) + '\n' + '\n');
-  fs.appendFileSync("README.md", "If you have any further questions, you can reach me at " + answers.email + " .");
-
-
-
+  fs.appendFileSync("README.md", "If you have any further questions, you can reach me at: " + answers.email);
 });
-// (function generateRM (data) {
-
-// fs.writeFile("README.md", JSON.stringify(data), function(err) {
-//   // ${data.projecttitle}
-
-//   // if (err) {
-//   //   return console.log(err);
-//   // }
-
-//   // console.log("Success!");
-
-
-// });
-// });
